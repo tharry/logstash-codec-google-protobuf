108762b4c2d44b0da273ca63c044138c4847b3d9
Piotr Haratym <piotr.haratym@adpilot.pl>
2017-03-29 15:00:55 +0200

    Google protobuf dependency added

--- lib/logstash/codecs/google-protobuf.rb
+++ lib/logstash/codecs/google-protobuf.rb
@@ -1,29 +1,13 @@
 # encoding: utf-8
 require "logstash/codecs/base"
 require "logstash/namespace"
+require 'google/protobuf'
 
-# This  codec will append a string to the message field
-# of an event, either in the decoding or encoding methods
-#
-# This is only intended to be used as an example.
-#
-# input {
-#   stdin { codec =>  }
-# }
-#
-# or
-#
-# output {
-#   stdout { codec =>  }
-# }
-#
+# TODO
 class LogStash::Codecs::GoogleProtobuf < LogStash::Codecs::Base
 
   # The codec name
   config_name "google-protobuf"
-
-  # Append a string to the message
-  config :append, :validate => :string, :default => ', Hello World!'
 
   def register
     @lines = LogStash::Codecs::Line.new
@@ -32,14 +16,14 @@ class LogStash::Codecs::GoogleProtobuf < LogStash::Codecs::Base
 
   def decode(data)
     @lines.decode(data) do |line|
-      replace = { "message" => line.get("message").to_s + @append }
+      replace = { "message" => line.get("message").to_s + 'append' }
       yield LogStash::Event.new(replace)
     end
   end # def decode
 
   # Encode a single event, this returns the raw data to be returned as a String
   def encode_sync(event)
-    event.get("message").to_s + @append + NL
+    event.get("message").to_s + 'append' + NL
   end # def encode_sync
 
 end # class LogStash::Codecs::GoogleProtobuf
--- logstash-codec-google-protobuf.gemspec
+++ logstash-codec-google-protobuf.gemspec
@@ -20,5 +20,6 @@ Gem::Specification.new do |s|
   # Gem dependencies
   s.add_runtime_dependency 'logstash-core-plugin-api', "~> 2.0"
   s.add_runtime_dependency 'logstash-codec-line'
+  s.add_runtime_dependency 'google-protobuf'
   s.add_development_dependency 'logstash-devutils'
 end
